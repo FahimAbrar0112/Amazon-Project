fa7ddacd984ef106766a90b69b30fd4586030092
FahimAbrar0112 <fahimabrar20@iut-dhaka.edu>
2024-07-10 03:56:57 +0600

    CLear Cart Button Functionality added .. and works fine

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, saveToStorage } from '../data/cart.js';
 import { products } from '../data/products.js';
 
 import { formatCurrency } from './utils/money.js';
@@ -112,4 +112,24 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
         console.log(container);
 
     });
+});
+
+// CLear Cart Button Functionality:
+document.querySelector('.js-clear-cart').addEventListener('click', () => {
+    cart.forEach((cartItem) => {
+        const productId = cartItem.productId;
+        removeFromCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+        container.remove();
+
+        console.log(container);
+    });
+
+
+
+
+    // cart.length = 0;
+    console.log(cart);
 });
